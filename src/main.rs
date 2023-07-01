7e4b9f29447d0990daeca6ef229a28b8a4c435ae
d-smith <doug.smith.mail@gmail.com>
2023-07-01 08:57:57 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    println!("Hello, world!");
+    dotenv::from_filename(".env").ok();
+
+    let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();
+    println!("{}",descriptor);
+}
